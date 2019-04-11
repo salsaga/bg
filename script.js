5374fbcf7a6c224b0adb9abcdffe0125f067b1f4
Gabriel <gabriel.salmeron@vantis.com>
2019-04-11 18:27:55 -0500

    backup 1

---
+++ script.js
@@ -0,0 +1,10 @@
+let whites = [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, 0, 0];
+let blacks = whites.reverse;
+let total = setTotal();
+
+let First = total.slice(0,5); Second = total.slice(6,11);
+let Third = total.slice(12, 17); Fourth = total.slice(18,23);
+
+function setTotal(){
+    return whites.map()
+}
